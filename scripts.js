6f8f38600bf1eb5a372f4e7e15083cb15da8db1b
GisliOskars <159532321+GisliOskars@users.noreply.github.com>
2024-02-19 10:24:59 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,6 +14,19 @@
     };
    
 
+    function display_c(){
+        var refresh=1000; // Refresh rate in milli seconds
+        mytime=setTimeout('display_ct()',refresh)
+     }
+     
+     function display_ct() {
+       var CDate = new Date()
+       var NewDate=CDate.toDateString(); 
+       NewDate = NewDate + " - " + CDate.toLocaleTimeString();
+       document.getElementById('ct').innerHTML = NewDate;
+       display_c();
+     }
+
 
 
 
